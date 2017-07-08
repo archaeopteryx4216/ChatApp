2625a515ef91243925ab8b168e1346e8052abc27
Alexander Cina-Bernard <jack42494@gmail.com>
2017-07-07 23:45:23 -0500

    Made a few more files.

--- server/chatapp.js
+++ server/chatapp.js
@@ -1,33 +1,14 @@
 'use strict';
 const
-
-  fs = require('fs'),
   net = require('net'),
-
-  filename = process.argv[2],
-
+  DataStore = require('./dataStore.js').DataStore,
+  dataStore = DataStore('file'),
   server = net.createServer(function(connection) {
-
-    // Reporting
-    console.log('Subscriber connected.');
-    connection.write("Now watching '"+filename+"' for changes...\n");
-
-    // Watcher setup
-    let watcher = fs.watch(filename, function() {
-      connection.write("File '"+filename+"' changed: "+Date.now()+"\n");
-    });
-
-    // Cleanup
-    connection.on('close', function() {
-      console.log("Subscriber disconnected.");
-      watcher.close();
+    
+    connection.on('', function() {
     });
 
   });
-
-if (!filename) {
-  throw Error('No target filename was specified.');
-}
 
 server.listen(5432, function() {
   console.log("Listening for subscribers...");
---
+++ server/dataStore.js
@@ -0,0 +1,12 @@
+"use strict";
+
+exports.DataStore = function(mode) {
+  var store = null;
+  if (mode === "file") {
+  }
+  else if (mode === "db") {
+  }
+  return store;
+}
+
+
